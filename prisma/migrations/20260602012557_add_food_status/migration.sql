4d11b10d4650f6c3d0c52c285c0b7282aeeab975
Auliyaaaaaaa <kuntum_adyah_33rpl@student.smktelkom-mlg.sch.id>
2026-06-02 09:03:42 +0700

    fix food status migration

--- prisma/migrations/20260602012557_add_food_status/migration.sql
+++ prisma/migrations/20260602012557_add_food_status/migration.sql
@@ -4,15 +4,9 @@
   - You are about to drop the `reservations` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE `reservations` DROP FOREIGN KEY `reservations_userId_fkey`;
-
 -- AlterTable
 ALTER TABLE `foods` ADD COLUMN `status` ENUM('PENDING', 'APPROVED', 'REJECTED') NOT NULL DEFAULT 'PENDING',
     ADD COLUMN `submittedBy` INTEGER NULL;
-
--- DropTable
-DROP TABLE `reservations`;
 
 -- AddForeignKey
 ALTER TABLE `foods` ADD CONSTRAINT `foods_submittedBy_fkey` FOREIGN KEY (`submittedBy`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
